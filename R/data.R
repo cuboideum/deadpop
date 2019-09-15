05fbf93ef47c4c73c174b9bdd741343fb4e3ba66
Felix Engel <Felix.Engel@uniklinik-freiburg.de>
2019-09-15 15:29:58 +0200

    Added documentation for data object 'sp_Pserrabs.mod1'.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Prevalence Calibration Model
+#'
+#' This is the model which is employed by the function \code{\link{Pcal}} to 
+#' calibrate prevalences of traits observed in partially preserved skeletal 
+#' populations.
+#'
+#' @format An R object of class \code{lm} as produced by the function 
+#'    \code{\link[stats]{lm}}.
+#'
+#' @source The data object was produced as part of Felix Engel's PhD thesis, 
+#'    entitled 'Traces of Armed Conflict: Methodological Implications from 
+#'    Anthropological Graveyard Analyses - Applied to the Site Lauchheim 
+#'    'Wasserfurche' With Special Reference to Cranial Trauma'.
+
+"sp_Pserrabs.mod1"
